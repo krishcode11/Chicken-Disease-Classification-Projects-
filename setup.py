ce7ceb853c78c662018527171f6c6bbfff9e74e7
krishcode11 <shaneraval239@gmail.com>
2026-05-27 17:05:14 +0530

    install all setups

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Chicken-Disease-Detection"
+AUTHOR_USER_NAME = "Krish"
+AUTHOR_EMAIL = "shaneraval239@gmail.com"
+SRC_REPO = "chicken-disease-detection"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for Chicken Disease Detection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{SRC_REPO}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{SRC_REPO}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
